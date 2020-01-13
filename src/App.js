3be64e14c9820834987e7058cf99749945d1477d
dwilliams64 <dwilliams_64@yahoo.com>
2020-01-13 10:10:15 -0600

    Took out comments

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ class App extends Component {
     super();
 
     this.state = {
-      // New property for state
+      
       monsters: [
 
         {
-          // React uses a key to keep track of objects
+          
           id: 1,
           name: 'Chris Redfield'
         },
@@ -39,14 +39,6 @@ class App extends Component {
     return (
       <div className="App">
         { 
-          // Key is an attribute used by React to keep track of each unique object
-
-          // We are using the map() method to access our state and print the names
-          // of our monsters as <h1> to the screen
-
-          // Usually when using .map() we will get a new array
-          // but React is smart enough to know that when we pass in HTML via JSX
-          // that it should render these elements as HTML elements
           this.state.monsters.map(monster => <h1 key={ monster.id }> { monster.name } </h1>)
         }
       </div>
@@ -55,19 +47,3 @@ class App extends Component {
 }
 
 export default App;
-
-/*
-
-React uses a key attribute to keep track of unique elements
-
-It does this because the data of an element can change but we can uses an unuiqe id to keep track of that element even though the data changed
-
-This is similar to our notes and todo app where we wanted to keep track of each object in an array using an id to identify that particular object
-
-React needs to know what needs to be updated if values changed and IDs help with this
-
-With IDs React to identify which element needs to be updated and only update that single element
-
-This helps prevent React from re rendering the entire DOM when one element changes
-
-*/
